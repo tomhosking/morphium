5848092c8fd5566d3b80ceae1635974812c567ee
Tom <tomhosking@gmail.com>
2017-04-11 14:38:04 +0100

    Trigger logic, without persistence. Some refactoring/spaghettifying.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,12 @@
 # ToDo
 
-* Settings - fields, link
+* Settings scene - UI, link, persistent store -> split into edit item and global prefs
+* Welcome/first time scene
 * Refactor out main widget into a component, link
 * Handle case where period has elapsed
 * Layout - flex not fixed
+* Trigger recalculations when appropriate
+
+* Shiny graphics per med
+* Log
+* iOS
--- index.android.js
+++ index.android.js
@@ -2,6 +2,8 @@
  * Morphium
  * https://github.com/tomhosking/morphium
  * @flow
+ *
+ * Dependencies: https://github.com/bgryszko/react-native-circular-progress
  */
 
 import React, { Component } from 'react';
@@ -13,7 +15,8 @@ import {
   TextInput,
   Navigator,
   TouchableHighlight,
-  BackAndroid
+  BackAndroid,
+  Button
 } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
@@ -42,7 +45,10 @@ export default class MorphiumApp extends Component {
               if (route.id == 'morphium.MorphiumSettingsView') {
 
                 return <MorphiumSettingsView nav={navigator} />
-            } else {
+            } else if (route.id == 'morphium.MorphiumHelpView') {
+
+              return <MorphiumHelpView nav={navigator} />
+          } else {
               return <MorphiumIndexView nav={navigator} />
             }
           }
@@ -63,13 +69,52 @@ export class MorphiumSettingsView extends Component {
         <Text style={styles.welcome}>
           Settins
         </Text>
-        <TextInput
-                style={{height: 40, width:200}}
-                placeholder="Drugname"
-                onChangeText={(text) => this.setState({text})}
-              />
-        </View>
+        <MorphiumItemEditView />
+        <Text onPress={() => {this.props.nav.pop()}}>Done</Text>
+      </View>
     );
+  }
+}
+
+export class MorphiumItemEditView extends Component {
+  constructor(props)
+  {
+    super(props)
+  }
+
+  render()
+  {
+    return (
+      <View>
+      <TextInput
+              style={{height: 40, width:200}}
+              placeholder="Drugname"
+              onChangeText={(text) => this.setState({text})}
+            />
+      <TextInput
+              style={{height: 40, width:200}}
+              placeholder="Interval"
+              onChangeText={(text) => this.setState({text})}
+            />
+      </View>
+    )
+  }
+}
+
+export class MorphiumHelpView extends Component {
+  constructor(props)
+  {
+    super(props)
+  }
+  render()
+  {
+    return(
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to Morphium</Text>
+        <Text>Morphium is a pain medication helper app</Text>
+        <Text onPress={() => {this.props.nav.pop()}}>Close</Text>
+      </View>
+    )
   }
 }
 
@@ -78,17 +123,11 @@ export class MorphiumIndexView extends Component {
   {
     super(props);
 
-    this.period = new Date(1970,0,1,4,0,0,0);
+    this.interval = new Date(1970,0,1,0,2,0,0);
     this.lastEvent = new Date(2017,3,10,10,30,0,0);
 
-    now = new Date();
-    this.state =  { timeRemaining: new Date(this.period - (now - this.lastEvent)), fillState: 100*(now - this.lastEvent)/this.period.getTime()}
-
-    console.log(this.state.timeRemaining.getTime());
-    console.log((now - this.lastEvent))
-    console.log(this.period.getTime())
-    console.log(this.state.fillState)
     // this.getSavedDate();
+
   }
 
 
@@ -98,22 +137,10 @@ export class MorphiumIndexView extends Component {
         <Text style={styles.welcome}>
           Morphium
         </Text>
-        <AnimatedCircularProgress
-  size={200}
-  width={30}
-  fill={this.state.fillState}
-  rotation={0}
-  tintColor="#3d5875"
-  backgroundColor="#00e0ff">
-  {
-    (fill) => (
-      <Text style={styles.points}>
-        { this.state.timeRemaining.getHours() + 'h ' + this.state.timeRemaining.getMinutes() + 'm' }
-      </Text>
-    )
-  }
-  </AnimatedCircularProgress>
+        <MorphiumItemWidget interval={this.interval} lastEvent={this.lastEvent} />
   <Text onPress={() => this.props.nav.push({id: 'morphium.MorphiumSettingsView'})}>Settings</Text>
+  <Text onPress={() => this.props.nav.push({id: 'morphium.MorphiumHelpView'})}>Help</Text>
+
   <Text style={styles.welcome}>
     {'Drugname'}
   </Text>
@@ -122,6 +149,91 @@ export class MorphiumIndexView extends Component {
   </Text>
       </View>
     );
+  }
+}
+
+export class MorphiumItemWidget extends Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    now = new Date();
+    this.state =  {
+      lastEvent: this.props.lastEvent,
+      timeRemaining: new Date(0),
+      fillState: 0,
+      enabled: false
+    }
+
+    this.onTrigger = this.onTrigger.bind(this)
+    this.onTick = this.onTick.bind(this)
+
+  }
+  componentDidMount()
+  {
+    this.onTick();
+
+  }
+  onTrigger()
+  {
+    let now = new Date()
+    console.log('On trigger! ' + now);
+    this.setState({'lastEvent': now});
+    this.setState({
+      'timeRemaining':  new Date(this.props.interval - (now - now)),
+      'fillState': Math.max(100*(now - now)/this.props.interval.getTime(), 100),
+      'enabled': this.props.interval - (now - now) < 0
+    })
+  }
+  onTick()
+  {
+    now = new Date();
+    this.setState({
+      'timeRemaining':  new Date(this.props.interval - (now - this.state.lastEvent)),
+      'fillState': Math.min(100*(now - this.state.lastEvent)/this.props.interval.getTime(), 100),
+      'enabled': this.props.interval - (now - this.state.lastEvent) < 0
+    })
+
+
+    console.log(this.state.timeRemaining);
+    console.log(this.state.lastEvent)
+    console.log(this.props.interval.getTime())
+    console.log(now - this.state.lastEvent)
+    console.log(this.state.fillState)
+    setTimeout(this.onTick, 1000);
+  }
+  render()
+  {
+
+    return (
+      <AnimatedCircularProgress
+        size={200}
+        width={30}
+        fill={this.state.fillState}
+        rotation={0}
+        tintColor={(this.state.enabled) ? "#00ff00" : "#3d5875" }
+        backgroundColor={(this.state.enabled) ? "#00e000" : "#00e0ff"  }>
+        {
+          (fill) => {
+            if(!this.state.enabled)
+            {
+              return (<Text style={styles.points}>
+              {this.state.timeRemaining.getHours() + 'h ' + this.state.timeRemaining.getMinutes() + 'm'}
+              </Text>)
+
+            }
+            else
+              {
+
+               return <Text style={styles.points} onPress={this.onTrigger}>Start</Text>
+
+               }
+          }
+        }
+      </AnimatedCircularProgress>
+
+  );
   }
 }
 
